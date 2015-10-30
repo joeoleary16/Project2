5fd3ced363e61b142bf98221f7cf0ef6b24e6b54
joeoleary16 <joeoleary@149.153.104.132>
2015-10-30 16:58:12 +0000

    Controller update

--- src/domain/Controller.java
+++ src/domain/Controller.java
@@ -1,49 +1,91 @@
 package domain;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Controller {
-	public static final String VERSION = "1.0";
-	private HashMap<String, Order> orders;
-	private HashMap<String, Service> services;
-	private ArrayList<Supplier> suppliers;
-	
-	public Controller() {
-		orders = new HashMap<String, Order>();
-		services = new HashMap<String, Service>();
-		suppliers = new ArrayList<Supplier>();
-	}
-	
-	/**
-	 * TODO: write javadoc
-	 * @param supplierID
-	 * @return
-	 */
-	public Supplier getSupplier(String supplierID) {
-		for(Supplier supplier : suppliers) {
-			if(supplier.getId().equals(supplierID)) {
-				return supplier;
-			}
-		}
-		return null; //TODO: something important
-	}
-	
-	/**
-	 * TODO: write javadoc
-	 * @param orderID
-	 * @return
-	 */
-	public Order getOrder(String orderID) {
-		return (orders.containsKey(orderID)) ? orders.get(orderID) : null;
-	}
+    public static final String VERSION = "1.0";
+    private HashMap<String, Order> orders;
+    private HashMap<String, Service> services;
+    private ArrayList<Supplier> suppliers;
+    private ArrayList<Location> locations;
 
-	public Service getService(String serviceID) {
-		return (services.containsKey(serviceID)) ? services.get(serviceID) : null;
-	}
-	
-	public String makeOrder(String staffID) {
-		Order o = new Order(staffID) ;
-		return o.getId() ;
-	}
+    public Controller() {
+        orders = new HashMap<String, Order>();
+        services = new HashMap<String, Service>();
+        suppliers = new ArrayList<Supplier>();
+        locations = new ArrayList<Location>();
+    }
+
+    public Supplier getSupplier(String supplierID) {
+        for (Supplier supplier : suppliers) {
+            if (supplier.getId().equals(supplierID))
+                return supplier;
+        }
+        return null;
+    }
+
+    public Location getLocation(String locationID) {
+        for (Location location : locations) {
+            if (location.getId().equals(locationID))
+                return location;
+        }
+        return null;
+    }
+
+    public Order getOrder(String orderID) {
+        return (orders.containsKey(orderID)) ? orders.get(orderID) : null;
+    }
+
+    public Service getService(String serviceID) {
+        return (services.containsKey(serviceID)) ? services.get(serviceID) : null;
+    }
+
+    public String makeOrder(String staffID) {
+        Order o = new Order(staffID);
+        orders.put(o.getId(), o);
+        return o.getId();
+    }
+
+    public void addItem(String orderID, String catalogID, String name, int quantity, double price, String description, String supplierID) {
+        Order o = getOrder(orderID);
+        Supplier s = getSupplier(supplierID);
+        o.addItem(s, catalogID, name, quantity, price, description);
+    }
+
+    public void attachDocument(String orderID, File document) {
+        Order o = getOrder(orderID);
+        o.setDocument(document);
+    }
+
+    public Order completeOrder(String orderID) {
+        return getOrder(orderID);
+    }
+
+    public void confirmOrder(String orderID) {
+        Order o = getOrder(orderID);
+        o.setDraft(false);
+    }
+
+    public String requestService(String staffID) {
+        Service s = new Service(staffID);
+        services.put(s.getId(), s);
+        return s.getId();
+    }
+
+    public boolean enterServiceDetails(String serviceID, String name, String description, double estTime, String locationID) {
+        Location l = getLocation(locationID);
+        Service s = getService(serviceID);
+        return s.setServiceDetails(name, description, estTime, l);
+    }
+
+    public Service editServiceDetails(String serviceID) {
+        return getService(serviceID);
+    }
+
+    public void confirmServiceRequest(String serviceID) {
+        Service s = getService(serviceID);
+        s.setDraft(false);
+    }
 }
